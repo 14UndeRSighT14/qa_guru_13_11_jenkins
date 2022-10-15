07be5ff30c22b6593501c63232234b7fd039b0b4
Янчиев В <yanchievvi@gmail.com>
2022-10-15 19:25:16 +0500

    Добавлены вложения

--- src/test/java/guru/qa/tests/demoqa/TestBase.java
+++ src/test/java/guru/qa/tests/demoqa/TestBase.java
@@ -2,7 +2,9 @@ package guru.qa.tests.demoqa;
 
 import com.codeborne.selenide.Configuration;
 import com.codeborne.selenide.logevents.SelenideLogger;
+import helpers.Attach;
 import io.qameta.allure.selenide.AllureSelenide;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.openqa.selenium.remote.DesiredCapabilities;
 
@@ -19,6 +21,13 @@ public class TestBase {
         Configuration.baseUrl = "https://demoqa.com";
         Configuration.browserSize = "1920x1080";
         Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub";
+    }
 
+    @AfterEach
+    void addAttachments() {
+        Attach.screenshotAs("Last screenshot");
+        Attach.pageSource();
+        Attach.browserConsoleLogs();
+        Attach.addVideo();
     }
 }
